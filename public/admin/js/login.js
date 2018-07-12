b9e62eb213a18b7b92d0eb3ccd9b33d655c706af
1935772416 <1935772416@qq.com>
2018-07-13 00:31:30 +0800

    登录页面完善

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,5 +1,5 @@
 $(function () {
-    // 表单校验  bootstrapValidator对表单有要求
+    // 1 表单校验  bootstrapValidator对表单有要求
     // 会在表单提交的时候做表单校验，如果校验失败 阻止表单提交 显示错误信息
 
     $("form").bootstrapValidator({
@@ -14,6 +14,9 @@ $(function () {
                         min:3,
                         max:6,
                         message:"用户名长度必须是3~6位"
+                    },
+                    callback:{
+                        message:"用户名不存在"
                     }
                 }
 
@@ -27,6 +30,9 @@ $(function () {
                         min:6,
                         max:12,
                         message:"密码长度为6~12位"
+                    },
+                    callback:{
+                        message:"密码错误"
                     }
                 }
             }
@@ -43,8 +49,35 @@ $(function () {
 
     })
 
+//  2 给表单注册一个检验成功的事件 
+   $("form").on("success.form.bv",function(e){
+       e.preventDefault();//阻止浏览器默认行为
 
+       $.ajax({ 
+           type:'post',
+           url:'/employee/employeeLogin',
+           data:$("form").serialize(),
+           success:function(info){
+               console.log(info);
+               if(info.error===1000){
+                //    console.log($("form").data("bootstrapvalidator"))
+                  $("form").data("bootstrapValidator").updateStatus("username","INVALID","callback");
+               }
+               if(info===1001){
+                $("form").data("bootstrapValidator").updateStatus("password","INVALID","callback");
+               }
+                 if(info.success){
+                     location.href = "index.html";
+                 }
+
+           }
+       })
+   })
   
+//    3 点击重置按钮 需要吧类容和样式全部清空  需要调用 bootstrapValidator
 
 
+    $("[type = 'reset']").on("click",function(){
+        $("form").data("bootstrapValidator").resetForm(true);
+    });
 })
